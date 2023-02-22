5c25f9299dd7d173308f2e00d82e22ab43680cf7
Nayana Mathew <nayanaansa@gmail.com>
2023-02-22 08:22:45 +0530

    commited file

---
+++ Remote.java
@@ -0,0 +1,10 @@
+package Demos;
+
+public class Remote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is remote job running code");
+	}
+
+}
